510e77bf6e4bad92221761988e8c1aa0a009607f
carolina-gn <caeng@iscte-iul.pt>
2025-10-08 14:53:18 +0100

    setup node.js + app express

--- server.js
+++ server.js
@@ -12,34 +12,3 @@ app.get('/Health', (req, res) => {
 app.listen(PORT, () => {
  console.log(`Server listening on port ${PORT}`);
 });
-
-app.get('/echo', async (req, res, next) => {
-    const {category, color } = req.query;
-
-    if (!category && !color) {
-        return res.status(400).json({
-        ok: false,
-        error: 'missing required query parameter: category, color'
-        });
-    }
-
-    res.json({ ok: true, category: category, color: color });
-});
-
-app.get('/error-route', (req, res) => {
-    throw new Error('something went wrong!');
-});
-
-app.use((err, req, res, next) => {
-    res.status(400).json({
-    ok: false,
-    error: err.message || 'internal server error',
-    });
-});
-
-
-
-
-    
-
-
