50ea7946fe9c8a8dd7dfbb44ac0a7529b7381081
carolina-gn <caeng@iscte-iul.pt>
2025-10-08 15:20:24 +0100

    update

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
  res.send('Hello World!');
-});
-
-app.get('/Health', (req, res) => {
- res.send('DEU!');
 });
 
 app.listen(PORT, () => {
