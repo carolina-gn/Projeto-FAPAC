69e9797319d3d1df9641a2f3d23570f99ae6dacc
Leonor Feliciano <96401019+leotheweirdo05@users.noreply.github.com>
2025-12-03 19:13:50 +0000

    feat: added dummy local login

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -6,13 +6,13 @@ const params = new URLSearchParams(window.location.search);
     const btnEl = document.getElementById('actionButton');
 
     if (status === 'success') {
-      titleEl.textContent = 'Login Successful';
-      msgEl.textContent = 'You can now enter the homepage.';
-      btnEl.textContent = 'Go to Homepage';
+      titleEl.textContent = 'Login bem sucedido!';
+      msgEl.textContent = '';
+      btnEl.textContent = 'Proceder para o Modelo';
       btnEl.onclick = () => window.location.href = '/';
     } else {
-      titleEl.textContent = 'Login Failed';
-      msgEl.textContent = 'Your username or password was incorrect.';
-      btnEl.textContent = 'Try Again';
+      titleEl.textContent = 'Erro de login!';
+      msgEl.textContent = 'O seu nome de utilizador ou palavra-passe estão incorretos.';
+      btnEl.textContent = 'Tentar Novamente';
       btnEl.onclick = () => window.location.href = '/login';
     }
